df0a1f10371eb51967f24dff00c66b549dcfffb3
whey <jimmy.hole@mail.ru>
2025-01-20 23:14:06 +0300

    lambda test commit

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -4,10 +4,10 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import com.codeborne.selenide.Condition;
 import static com.codeborne.selenide.Selectors.withText;
+import static io.qameta.allure.Allure.step;
 
 
-
-public class SimpleTest {
+public class SimpleTest extends TestBase {
     @Test
     public void simpleGHTest(){
         //SelenideLogger.addListener("allure", new AllureSelenide());
@@ -20,4 +20,24 @@ public class SimpleTest {
         $("#issues-tab").click();
         $(withText("ForTestIssue")).should(Condition.exist);
     }
+    @Test
+    public void lambdaTest(){
+        step("Open main page", () ->{
+            open("https://github.com");
+        });
+        step("Search our repository" +" "+ REPOSITORY, () ->{
+            $(".search-input").click();
+            $("#query-builder-test").sendKeys("Jaliz9087/AllureNHWRepo");
+            $("#query-builder-test").submit();
+        });
+        step("Found our repo", () ->{
+            $(By.linkText("Jaliz9087/AllureNHWRepo")).click();
+            $("#issues-tab").click();
+        });
+        step("Checking the header"+" " + TITLE, () ->{
+            $(withText("ForTestIssue")).should(Condition.exist);
+        });
+
+    }
 }
+
---
+++ src/test/java/TestBase.java
@@ -0,0 +1,4 @@
+public class TestBase {
+    public static final String REPOSITORY = "Jaliz9087/AllureNHWRepo";
+    public static final String TITLE = "ForTestIssue";
+}
